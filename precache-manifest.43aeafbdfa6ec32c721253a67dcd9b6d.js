9805ac18e4da23e08ab4ee3ed0f6d883098c7a07
Peter Shevchuk <raf02041994@gmail.com>
2020-08-18 20:32:43 +0300

    Updates

---
+++ precache-manifest.43aeafbdfa6ec32c721253a67dcd9b6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5481284b206ea93ee39e2b754bed28b",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "98466dc402ecb3f8a309",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.e6773652.chunk.css"
+  },
+  {
+    "revision": "3d57cf9adb0d080e767d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.9ceda5cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.9ceda5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98466dc402ecb3f8a309",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.89740913.chunk.js"
+  },
+  {
+    "revision": "8071d960000cc3de9bda",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.4230262a.js"
+  }
+]);
